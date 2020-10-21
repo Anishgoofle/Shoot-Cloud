9e8f7cd0366bc9dd31a9b744fa64aef0490f61b2
Anish Ojha <anishojha82@gmail.com>
2020-10-21 16:03:19 +0530

    seup projectile shooting from canvas element

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas');
 
 const ctx = canvas.getContext('2d');
 
@@ -16,14 +16,87 @@ class Player {
 
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this, y, this.radius, 0, Math.PI * 2, false);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
     }
 }
 
+
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+
+const projectiles = [];
+const enemies = [];
+
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 });
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(100, 100, 30, 'blue');
-player.draw();
+const player = new Player(x, y, 30, 'blue');
+
+function spawnEnemies() {
+    setInterval(() => {
+
+    }, 1000);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach(projectile => projectile.update());
+}
+
+
+addEventListener('click', (e) => {
+    const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2);
+    const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity));
+});
+animate();
