e9c9d0fd7b4b857da60dabe903d869c4aaa15ef0
Anish Ojha <anishojha82@gmail.com>
2020-10-22 13:15:04 +0530

    added game over scenario

--- index.js
+++ index.js
@@ -102,8 +102,9 @@ function spawnEnemies() {
     }, 1000);
 }
 
+let animationId;
 function animate() {
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw();
     projectiles.forEach((projectile) => {
@@ -111,6 +112,12 @@ function animate() {
     });
     enemies.forEach((enemy, index) => {
         enemy.update();
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId);
+        }
 
         projectiles.forEach((projectile, proIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
